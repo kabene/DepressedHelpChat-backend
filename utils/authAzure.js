59b9b41ced697209528182cb065cd0a58dcc56a7
Karim Abene <32820740+kabene@users.noreply.github.com>
2022-05-03 15:04:23 +0200

    Update authAzure.js

--- utils/authAzure.js
+++ utils/authAzure.js
@@ -8,7 +8,7 @@
 
 
 const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics");
-const key = '4035600fb37547a0b4d32c5fc7bc69c7';
+const key = '';
 const endpoint = 'https://depressedhelpedchat.cognitiveservices.azure.com/';
 const textAnalyticsClient = new TextAnalyticsClient(endpoint,  new AzureKeyCredential(key));
 
